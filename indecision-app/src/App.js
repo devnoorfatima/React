8b3e95bc8000b6aa312d7a5b2f3cf955896f191e
devnoorfatima <devnoorfatima@gmail.com>
2019-02-11 19:41:47 +0500

    Made a counter that causes increment and decrement of the in the number and resets it.

--- indecision-app/src/App.js
+++ indecision-app/src/App.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
-import Es6 from "./ES6-let-const"
-import Arrow from "./ES6-arrow-functions"
-import Arrow2 from "./ES6-arrow-functions-2"
+// import React, { Component } from 'react';
+// import Es6 from "./ES6-let-const"
+// import Arrow from "./ES6-arrow-functions"
+// import Arrow2 from "./ES6-arrow-functions-2"
+import Counter from "./Counter"
 import './App.css';
 
 //Conditional Rendering
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
  
-const user = {
-  name : "Noor",
-  age : 27,
-  location: "Lahore"
-};
+// const user = {
+//   name : "Noor",
+//   age : 27,
+//   location: "Lahore"
+// };
 
-function getLocation(location) {
-  if(location){
-    return <p>location:{location}</p>;
-  }
-}
-    return (      
-      <div className="App">
-          <h1>{user.name ? user.name : "Anonymous"}</h1>
-          {user.age >= 18 && <p>Age : {user.age}</p>}
-          {getLocation(user.location)}
+// function getLocation(location) {
+//   if(location){
+//     return <p>location:{location}</p>;
+//   }
+// }
+//     return (      
+//       <div className="App">
+//           <h1>{user.name ? user.name : "Anonymous"}</h1>
+//           {user.age >= 18 && <p>Age : {user.age}</p>}
+//           {getLocation(user.location)}
       
-      <Es6 />
-      <Arrow />
-      <Arrow2 />
-      </div>
-    );
-  }
-}
+//       <Es6 />
+//       <Arrow />
+//       <Arrow2 />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+export default Counter;
---
+++ indecision-app/src/Counter.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+class Count extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+  
+    // change code below this line
+  
+    increment() {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
+    
+    decrement() {
+      this.setState({
+        count: this.state.count - 1
+      });
+    };
+  
+    reset() {
+      this.setState({
+        count: 0
+      });
+    };
+  
+    // change code above this line
+    render() {
+      return (
+  
+     <div>
+      <h1>Current Count: {this.state.count}</h1>
+      <button className='inc' onClick={(e) => this.increment(e)}>+1</button>
+      <button className='dec' onClick={(e) => this.decrement(e)}>-1</button>
+      <button className='reset' onClick={(e) => this.reset(e)}>Reset</button>
+    </div>
+      );
+    }
+  };
+export default Count;
