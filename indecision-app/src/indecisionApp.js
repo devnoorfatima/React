827736c023bc9c99cff0336a965cfbf1261582ae
devnoorfatima <devnoorfatima@gmail.com>
2019-02-18 16:16:14 +0500

    Practising react basics.

--- indecision-app/src/indecisionApp.js
+++ indecision-app/src/indecisionApp.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
 class Form extends Component {
-    render() {  
+  state = {
+    option : 0
+  }
+  render() {  
+    
     const user = {
       title: "Indecision App",
       subtitle: "Enjoy your life with computer",
-      options: ["one", "two"]
+      options: []
     };
     const onFormSubmit = (e) => {
       e.preventDefault();
-
        const option = e.target.elements.option.value;
 
        if(option){
          user.options.push(option);
-         e.target.elements.option.value = "";
-         console.log(user.options);
-       }
+        e.target.elements.option.value = '';
+        }
+    console.log(user.options);
     };
     const onRemoveAll = () => {
      user.options = [];
+    };
+    const onMakeDecision = () => {
+      const randomNum = Math.floor(Math.random() * user.options.length);
+      console.log(randomNum);
     };
       return (      
         <div className="form">
         <h1>{user.title}</h1>
         {user.subtitle && <p>{user.subtitle}</p>}        
         <p>{user.options.length > 0 ? "Here are your options" : "No options"}</p>
-        <p>{user.options.length}</p>
+         <button onClick = {onMakeDecision}>What should I do?</button>
         <button onClick={onRemoveAll}>Remove All</button>
-        <ol>
-          <li>Item one</li>
-          <li>Item two</li>
-        </ol>
+        <ol> {
+          user.options.map((option) => {
+            return <li key = {option}>{option}</li>
+          })
+        }
+       </ol>
         <form onSubmit = {onFormSubmit}>
           <input type="text" name="option" />
           <button>Add Option</button>
