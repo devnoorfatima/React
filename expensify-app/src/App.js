a5096a3e3ea89ef82979626a5f97f8a9b17c59ce
devnoorfatima <devnoorfatima@gmail.com>
2019-03-01 19:07:46 +0500

    Making a multi-page app so set up the routes and components for the app.

--- expensify-app/src/App.js
+++ expensify-app/src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import 'normalize.css/normalize.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 // import './App.css';
 const ExpenceDashboardPage = () => (
   <div>
     This is from my dashboard.
   </div>
-)
-const routes =  (
-<BrowserRouter>
-<Route path ='/' component={ExpenceDashboardPage} />
-</BrowserRouter>
-  );
+);
+
+const AddExpencePage = () => (
+  <div>
+    This is from my AddExpencePage.
+  </div>
+);
+
+const EditPage = () => (
+  <div>
+    This is from my EditPage.
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my HelpPage.
+  </div>
+);
 class App extends Component {
   render() {
     return (
       <div className="App">
-      This is from my dashboard.
+      <BrowserRouter>
+<Switch>
+<Route path ='/' component={ExpenceDashboardPage} exact={true} />
+<Route path ='/create' component={AddExpencePage} />
+<Route path ='/edit' component={EditPage} />
+<Route path ='/help' component={HelpPage} />
+</Switch>
+</BrowserRouter>
       </div>
     );
   }
