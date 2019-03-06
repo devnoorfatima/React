cca5f99cfcdcd1fb455638410bbd3d945c21474e
devnoorfatima <devnoorfatima@gmail.com>
2019-03-06 23:23:16 +0500

    Practising redux basics also object and array destructuring in ES6.

--- expensify-app/src/redux.js
+++ expensify-app/src/redux.js
@@ -1,25 +1,74 @@
 import {createStore} from 'redux';
+// Destructuring  objects
+// const book= {
+//     title : "hello",
+//     author: "finny",
+//     publisher:{
+//         name: 'blu'
+//     }
+// };
+
+// const {name = 'self-published'}= book.publisher;
+// console.log(name);
+
+// Destructuring  objects
+const  item = ["coffee(iced)","$2","$2.50","$2.75"];
+const [coffee, ,medium, ] = item
+console.log(`A medium ${coffee} costs ${medium}.`)
+
 const Store = createStore((state = {count : 0}, action) => {
 switch (action.type){
     case 'INCREMENT':
-     return {
-        count : state.count + 1
+    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+    return {
+        count : state.count + incrementBy
     };
 
+   //Reset 
+
+   case 'RESET':
+   return {
+      count : 0
+  };
+
+
     case 'DECREMENT':
+    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1; 
      return {
-        count : state.count - 1
+        count : state.count - decrementBy
     };
     
     default:
     return state;
 }   
 });
+
+Store.subscribe(()=> {
 console.log(Store.getState());
+})
+Store.dispatch(
+    {
+        type: 'INCREMENT',
+        incrementBy: 5
+    }
+);
 
 Store.dispatch(
     {
         type: 'INCREMENT'
+    }
+);
+Store.dispatch(
+    {
+        type: 'RESET'
+    }
+);
+
+
+Store.dispatch(
+    {
+        type: 'DECREMENT',
+        decrementBy : 10
     }
 );
 
@@ -29,7 +78,4 @@ Store.dispatch(
         type: 'DECREMENT'
     }
 );
-
-console.log(Store.getState());
-
 export default Store;
