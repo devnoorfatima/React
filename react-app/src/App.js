da4b7ace2fe16732db18ec9c3fd23efa7e47aa40
Noor Fatima <devnoorfatima@gmail.com>
2019-04-24 22:54:56 +0500

    Started practicing react.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Todos from './components/todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    todos:[
+      {
+        id:1,
+        title:'get food',
+        completed:false
+      },
+      {
+        id:2,
+        title:'unpack it',
+        completed:false
+      },
+      {
+        id:3,
+        title:'eat',
+        completed:false
+      }
+    ]
+  }
+  render() {
+    return (
+      <div>
+     <Todos todos={this.state.todos} />
+      </div>
+    );
+  
+  }
 }
+
+  
 
 export default App;
---
+++ react-app/src/components/todos.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Todos extends Component{
+    render() {
+        
+        return this.props.todos.map((todo) => (
+            <h3>{todo.title}</h3>
+             )); 
+    }
+  }
+  
+export default Todos;
