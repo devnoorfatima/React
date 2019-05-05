9371b4dae8320d2e78341494214a32a5c846cd3a
Noor Fatima <devnoorfatima@gmail.com>
2019-05-05 19:58:12 +0500

    Working on weather app.

--- weather-app/src/Components/temperature.js
+++ weather-app/src/Components/temperature.js
@@ -2,18 +2,22 @@ import React, {Component} from 'react';
 import axios from 'axios';
 class Temperature extends Component {
    state={
-       temperature:[]
+       weather:[]
    }
 
    componentDidMount() {
-       axios.get("https://fcc-weather-api.glitch.me/api/current?lat="+this.props.lat+ "&lon="+this.props.lon)
-       .then(res=> this.setState({temperature:res.data}))
+       let latitude = this.props.lat;
+       let longitude = this.props.lon;
+
+       axios.get('https://fcc-weather-api.glitch.me/api/current?lat='+latitude+'&lon='+longitude)
+       .then(res=> this.setState({weather:res.data}))
    }
     render() {
         return (
             <div>
-                <h2>{this.props.lon}</h2>
-                <h2>{this.props.lat}</h2>
+                {/* <h2>{this.props.lon}</h2>
+                <h2>{this.props.lat}</h2> */}
+                {/* <h2>{this.state.weather.main.temp}</h2> */}
             </div>
         ) 
     }
