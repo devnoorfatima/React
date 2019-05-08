434308c48c9b82f161194aade02a78ee2c0fc412
Noor Fatima <devnoorfatima@gmail.com>
2019-05-08 23:22:53 +0500

    making my api work  to get temperature.

--- weather-app/src/Components/temperature.js
+++ weather-app/src/Components/temperature.js
@@ -4,22 +4,21 @@ class Temperature extends Component {
    state={
        weather:[]
    }
-
    componentDidMount() {
-       let latitude = this.props.lat;
-       let longitude = this.props.lon;
-
-       axios.get('https://fcc-weather-api.glitch.me/api/current?lat='+latitude+'&lon='+longitude)
-       .then(res=> this.setState({weather:res.data}))
-   }
+       
+    axios.get('api.openweathermap.org/data/2.5/weather?lat='+this.props.lat+'&lon='+this.props.lon)
+    .then(res=> this.setState({location:res.data}))
+}
     render() {
+        
         return (
             <div>
-                {/* <h2>{this.props.lon}</h2>
-                <h2>{this.props.lat}</h2> */}
+                
                 {/* <h2>{this.state.weather.main.temp}</h2> */}
             </div>
         ) 
     }
   }
   export default Temperature;
+//   eb04322a785360de28790da75e8bf586
+// https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22 
